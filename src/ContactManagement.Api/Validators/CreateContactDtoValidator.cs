9757170cbecca136dbc0d74afb85e42b6cdd71da
Ahmed Khatab <Ahemdkhatab7070@gmail.com>
2026-01-30 18:52:21 +0200

    Add validation rules

---
+++ src/ContactManagement.Api/Validators/CreateContactDtoValidator.cs
@@ -0,0 +1,39 @@
+using ContactManagement.Api.DTOs.Contacts;
+using FluentValidation;
+
+namespace ContactManagement.Api.Validators;
+
+public class CreateContactDtoValidator : AbstractValidator<CreateContactDto>
+{
+    public CreateContactDtoValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .MaximumLength(100).WithMessage("First name must not exceed 100 characters");
+        
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            .MaximumLength(100).WithMessage("Last name must not exceed 100 characters");
+        
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format")
+            .MaximumLength(255).WithMessage("Email must not exceed 255 characters");
+        
+        RuleFor(x => x.PhoneNumber)
+            .MaximumLength(20).WithMessage("Phone number must not exceed 20 characters")
+            .Matches(@"^[\d\s\+\-\(\)]*$").WithMessage("Invalid phone number format")
+            .When(x => !string.IsNullOrEmpty(x.PhoneNumber));
+        
+        RuleFor(x => x.BirthDate)
+            .LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Today))
+            .WithMessage("Birth date cannot be in the future")
+            .When(x => x.BirthDate.HasValue);
+        
+        RuleFor(x => x.Address)
+            .MaximumLength(500).WithMessage("Address must not exceed 500 characters");
+        
+        RuleFor(x => x.Notes)
+            .MaximumLength(1000).WithMessage("Notes must not exceed 1000 characters");
+    }
+}
---
+++ src/ContactManagement.Api/Validators/LoginDtoValidator.cs
@@ -0,0 +1,17 @@
+using ContactManagement.Api.DTOs.Auth;
+using FluentValidation;
+
+namespace ContactManagement.Api.Validators;
+
+public class LoginDtoValidator : AbstractValidator<LoginDto>
+{
+    public LoginDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+        
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required");
+    }
+}
